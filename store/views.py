68d84246f2c9454743632132d91136cd2b71c6b9
pakofdz <paco.fdz.2001@gmail.com>
2023-12-11 18:01:08 -0600

    MAIN

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
 import json
 import datetime
 
@@ -36,6 +39,24 @@ def checkout(request):
 
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/checkout.html', context)
+
+def rates(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'store/rates.html', context)
+
+def login(request):
+    data = cartData(request)
+    cartItems = data['cartItems']
+    order = data['order']
+    items = data['items']
+
+    context = {'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'store/login.html', context)
 
 
 def updateItem(request):
