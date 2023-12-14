60917a812c57a8996dae06791840a3f653469016
pakofdz <paco.fdz.2001@gmail.com>
2023-12-14 01:13:47 -0600

    no customer

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def login_user(request):
@@ -22,3 +23,25 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("Te haz deslogeado!"))
     return redirect('store')
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request,("Registration successful!"))
+
+            user_model = User.objects.get(username=name)
+            new_customer = Customer.objects.create(user=user_model, name=name, email=email)
+            new_customer.save()
+
+            return redirect('store')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authenticate/register.html', {
+        'form':form,
+        })
